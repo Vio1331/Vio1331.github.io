d0c4704ab8a9b0d1be3990c807004a5c4498abe0
Vio <chinajjzty97@gmail.com>
2026-09-09 21:37:47 +0800

    Expose layout read errors in integration tests

--- tools/content-build/test/journal_test.rb
+++ tools/content-build/test/journal_test.rb
@@ -17,7 +17,7 @@ class JournalTest < Minitest::Test
       FileUtils.cp_r("#{ROOT}/_includes", directory)
       FileUtils.cp("#{ROOT}/feed.xml", directory)
       config = Jekyll.configuration({
-        'source' => directory, 'destination' => "#{directory}/_site", 'quiet' => true,
+        'source' => directory, 'destination' => "#{directory}/_site", 'quiet' => false, 'strict_front_matter' => true,
         'url' => 'https://example.com', 'baseurl' => baseurl, 'lang' => 'zh-CN',
         'title' => '示例网站', 'author' => {'name' => 'Vio'},
         'collections' => {'journal' => {'output' => true, 'permalink' => '/journal/:name/'}},
@@ -67,11 +67,11 @@ class JournalTest < Minitest::Test
     MD
     with_site({'20260102_示例' => source("image_base: /assets/images/journal/示例/\ncover: cover.jpg\n", body)}, '/sub') do |site, directory|
       html = Nokogiri::HTML(File.read("#{directory}/_site/journal/示例/index.html"))
-      assert html.at_css('.journal-content'), "Journal layout missing: #{html.to_html}; data=#{site.collections['journal'].docs.first.data.inspect}; content=#{site.collections['journal'].docs.first.content.inspect}; output=#{site.collections['journal'].docs.first.output.inspect}; layouts=#{site.layouts.transform_values { |l| l.content[0,150] }.inspect}"
+      assert html.at_css('.journal-content'), "Journal layout missing: #{html.to_html}; data=#{site.collections['journal'].docs.first.data.inspect}; content=#{site.collections['journal'].docs.first.content.inspect}; output=#{site.collections['journal'].docs.first.output.inspect}; layouts=#{site.layouts.transform_values { |l| {path: l.path, content: l.content.to_s[0,150]} }.inspect}"
       images = html.css('.journal-content img').map { |n| n['src'] }
       assert_equal 4, images.length, "Rendered journal content: #{html.at_css('.journal-content').to_html}"
       assert_equal ['/sub/assets/images/journal/示例/照片 01.jpg', '/sub/assets/images/journal/示例/second.jpg', '/sub/assets/shared.png', 'https://example.net/out.png'], images.map { |s| URI::DEFAULT_PARSER.unescape(s) }
-      assert_equal '/sub/assets/images/journal/示例/cover.jpg', html.at_css('.journal-cover img')['src']
+      assert_equal '/sub/assets/images/journal/示例/cover.jpg', URI::DEFAULT_PARSER.unescape(html.at_css('.journal-cover img')['src'])
       assert_equal 'notes.html', html.at_css('.journal-content a')['href']
       assert_includes html.at_css('.journal-content').text, '<img src="code.png">'
       assert_includes html.at_css('.journal-content').text, '<img src="block.png">'
